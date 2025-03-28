6998fe394cd54ee758980c20a07316280052fd41
kristjan kangro <kristjan.kangro@visma.com>
2025-03-28 22:30:13 +0200

    railway

---
+++ ErrorsAndFailures/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace ErrorsAndFailures
+{
+    public class Customer
+    {
+        private decimal Balance { get; set; }
+        public string BillingInfo { get; private set; }
+
+        public void AddBalance(MoneyToCharge amount)
+        {
+            Balance += amount;
+        }
+    }
+}
